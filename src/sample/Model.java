1ee4dd48d654ec8b5304e240a6620a2affecc254
chetaldrich <chetaldrich@gmail.com>
2014-11-14 10:38:11 -0600

    Add methods Model

--- src/sample/Model.java
+++ src/sample/Model.java
@@ -1,14 +1,49 @@
 package sample;
 
 
+import java.util.List;
+
 public class Model {
+
+    private List asteroids;
+    private Spaceship spaceship;
 
     public Model() {
 
     }
-    public void checkShipAsteroidCollision(){}
-    public void checkAsteroidBulletCollision(){}
-    public void Update(){
-        
+
+    public void checkShipAsteroidCollision(){
+        //stub :)
     }
+
+    public void checkAsteroidBulletCollision(){
+        // stub :)
+    }
+
+    public void generateAsteroid(){
+        // add asteroid to list
+    }
+
+    public void generateBullet(){
+
+    }
+
+
+    public void createNewShip(){
+
+    }
+
+    public void moveShip(){
+
+    }
+
+    public void updateLives(){
+
+    }
+
+    public void updateScore(){
+
+    }
+
+
 }
