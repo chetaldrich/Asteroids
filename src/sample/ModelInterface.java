cc7c1105b5f7f87bb1b59e868bb7777ddb615404
chetaldrich <chetaldrich@gmail.com>
2014-11-14 11:17:58 -0600

    Update Modelinterface, Model with new methods

--- src/sample/ModelInterface.java
+++ src/sample/ModelInterface.java
@@ -1,128 +1,90 @@
 package sample;
 
-
 import java.util.List;
 
-public class Model {
-
-    private List asteroids;
-    private Spaceship spaceship;
-    private List bullets;
-    private Scoreboard scoreboard;
-
-    /**
-     * Model Constructor
-     * Will create a beginning game state in the absence of one.
-     */
-    public Model() {
-        //stub :)
-    }
+public interface ModelInterface {
 
     /**
      * checkShipAsteroidCollision -- checks for a Ship and Asteroid collision, updates Lives on collision.
      * @return void
      */
-    public void checkShipAsteroidCollision(){
-        //stub :)
-    }
+    public void checkShipAsteroidCollision();
 
     /**
      * checkAsteroidBulletCollision -- checks for a Ship and Asteroid collision, removes both objects from lists on collision.
      * @return void
      */
-    public void checkAsteroidBulletCollision(){
-        // stub :)
-    }
+    public void checkAsteroidBulletCollision();
 
     /**
      * generateAsteroid -- creates a new Asteroid instance, and adds it to the current asteroidList.
      * @return void
      */
-    public void generateAsteroid(){
-        // add asteroid to list
-    }
+    public void generateAsteroid();
 
     /**
      * generateBullet -- creates a new Bullet instance, and adds it to the current bulletList.
      * @return void
      */
-    public void generateBullet(){
-        //stub :)
-    }
+    public void generateBullet();
 
 
     /**
      * createNewShip -- creates a new Spaceship instance in the event of destruction due to collision or game initialization.
      * @return void
      */
-    public void createNewShip(){
-        //stub :)
-    }
+    public void createNewShip();
 
     /**
      * moveShip -- changes Ship velocity on method call (will be called from Controller on keypress).
      * @return void
      */
-    public void moveShip(){
-        //stub :)
-    }
+    public void moveShip();
 
     /**
      * getSpaceShip -- gets the current Spaceship instance in the model.
      * @return Spaceship current spaceship
      */
-    public Spaceship getSpaceship(){
-        return spaceship;
-    }
+    public Spaceship getSpaceship();
 
     /**
      * getAsteroidList -- gets the current asteroidList in the model.
      * @return List current active asteroids
      */
-    public List getAsteroidList(){
-        return asteroids;
-    }
+    public List getAsteroidList();
 
     /**
      * getBulletList -- gets the current bulletList in the model.
      * @return List current active bullets
      */
-    public List getBulletList(){
-        return bullets;
-    }
+    public List getBulletList();
 
     /**
      * getScoreboard -- gets the current Scoreboard in the model.
      * @return Scoreboard scoreboard
      */
-    public Scoreboard getScoreboard(){
-        return scoreboard;
-    }
+    public Scoreboard getScoreboard();
 
     /**
      * createScoreboard --  creates the Scoreboard.
      * @return void
      */
-    public void createScoreboard(){
-        //stub :)
-    }
+    public void createScoreboard();
 
     /**
      * updateLives -- updates current lives based on other method triggers, e.g. collision.
+     * @param lives number of lives to add or deduct
      * @return void
      */
-    public void updateLives(int lives){
-        //stub :)
-    }
+    public void updateLives(int lives);
 
     /**
      * updateScore -- updates current Score based on method triggers (survival time, bullet Asteroid collision)
+     * @param points number of points to add or deduct
      * @return void
      */
-    public void updateScore(int scoreAddition){
-        //stub :)
-    }
+    public void updateScore(int points);
 
-
+    p
 
 }
