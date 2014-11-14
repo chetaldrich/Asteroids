6b3a085c5a72e11c0992662762b9e4fcd4efee84
Josie Bealle <beallej@carleton.edu>
2014-11-14 11:03:18 -0600

    model test stuff

--- src/sample/tests/ModelTest.java
+++ src/sample/tests/ModelTest.java
@@ -1,9 +1,12 @@
 package sample.tests;
 
+import javafx.geometry.Point2D;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 import sample.*;
+
+import java.util.List;
 
 public class ModelTest {
 
@@ -14,16 +17,74 @@ public class ModelTest {
     @Test
     public void testCheckShipAsteroidCollision() throws Exception {
         Model model = new Model();
-        Asteroid asteroid= new Asteroid();
-        Spaceship spaceship = new Spaceship();
+        model.generateAsteroid();
+        model.createNewShip();
+        Spaceship spaceship = model.getSpaceship();
+        List<Asteroid> asteroids = model.getAsteroidList();
+        Point2D spaceshipLocation = spaceship.getPosition();
+        double spaceshipLocationX = spaceshipLocation.getX();
+        double spaceshipLocationY = spaceshipLocation.getY();
+
+        asteroids.get(0).setPosition(spaceshipLocationX, spaceshipLocationY);
+
+        assertTrue("Model checkShipAsteroidCollision() Fail", model.checkShipAsteroidCollision());
 
     }
 
     @Test
     public void testCheckAsteroidBulletCollision() throws Exception {
         Model model = new Model();
-        Asteroid asteroid= new Asteroid();
-        Bullet bullet = new Bullet();
+        model.generateAsteroid();
+        model.generateBullet();
+        List<Bullet> bullets = model.getBullets();
+        List<Asteroid> asteroids = model.getAsteroidList();
+        Point2D firstBulletLocation = bullets.get(0).getPosition();
+        double firstBulletLocationX = firstBulletLocation.getX();
+        double firstBulletLocationY = firstBulletLocation.getY();
+
+        asteroids.get(0).setPosition(firstBulletLocationX, firstBulletLocationY);
+
+        assertTrue("Model checkAsteroidBulletCollision() Fail", model.checkAsteroidBulletCollision());
+
+    }
+
+    @Test
+    public void testGenerateAsteroid(){
+        Model model = new Model();
+        model.generateAsteroid();
+        assertTrue("Model generateAsteroid() Fail", 1==model.getAsteroidList().size());
+    }
+
+    @Test
+    public void testGenerateBullet(){
+        Model model = new Model();
+        model.generateBullet();
+        assertTrue("Model generateBullet() Fail", 1==model.getBulletList().size());
+
+    }
+
+    @Test
+    public void testCreateNewShip(){
+        Model model = new Model();
+        model.createNewShip();
+        assertNotNull("Model createNewShip() Fail", model.getSpaceship());
+    }
+
+    @Test
+    public void testMoveShip(){
+        
+
+
+    }
+
+    @Test
+    public void testUpdateLives(){
+
+    }
+
+    @Test
+    public void testUpdateScore(){
+
     }
 
 }
