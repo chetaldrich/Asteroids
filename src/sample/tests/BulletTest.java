dec7478c7dd7149571cb403a1eca272120ea945b
Josie Bealle <beallej@carleton.edu>
2014-11-16 16:24:33 -0600

    bullet test quick fix

--- src/sample/tests/BulletTest.java
+++ src/sample/tests/BulletTest.java
@@ -20,14 +20,14 @@ public class BulletTest {
 
     @Test
     public void testGetPosition() throws Exception {
-        Bullet bullet = new Bullet(10,10);
+        Bullet bullet = new Bullet(new Point2D(10,10));
         bullet.setPosition(2,2);
         assertEquals("Bullet getPosition Fail", new Point2D(2,2), bullet.getPosition());
     }
 
     @Test
     public void testSetPosition() throws Exception {
-        Bullet bullet = new Bullet(10,10);
+        Bullet bullet = new Bullet(new Point2D(10,10));
         bullet.setPosition(4,4);
         assertEquals("Bullet setPosition Fail", new Point2D(4,4), bullet.getPosition());
     }
@@ -36,14 +36,14 @@ public class BulletTest {
 
     @Test
     public void testGetVelocity() throws Exception {
-        Bullet bullet = new Bullet(10,10);
+        Bullet bullet = new Bullet(new Point2D(10,10));
         bullet.setVelocity(2,2);
         assertEquals("Bullet getVelocity Fail", new Point2D(2,2), bullet.getVelocity());
     }
 
     @Test
     public void testSetVelocity() throws Exception {
-        Bullet bullet = new Bullet(10,10);
+        Bullet bullet = new Bullet(new Point2D(10,10));
         bullet.setVelocity(4,4);
         assertEquals("Bullet setVelocity Fail", new Point2D(4,4), bullet.getVelocity());
 
@@ -51,7 +51,7 @@ public class BulletTest {
 
     @Test
     public void testStep() throws Exception {
-        Bullet bullet = new Bullet(10,10);
+        Bullet bullet = new Bullet(new Point2D(10,10));
         Point2D oldPosition = bullet.getPosition();
         double xPosition = oldPosition.getX();
         double yPosition = oldPosition.getY();
