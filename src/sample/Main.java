ef7c9c1f1e7e35f1f6252fd9aa564a8e32a220cd
Josie Bealle <beallej@carleton.edu>
2014-11-18 20:31:56 -0600

    main ctrl

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,9 +1,11 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 
@@ -15,9 +17,15 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
         Parent root = (Parent)loader.load();
-        Controller controller = loader.getController();
+        final Controller controller = loader.getController();
         // Here, call whatever methods you want to call on controller.
 
+        root.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                controller.handleKeyPress(keyEvent);
+            }
+        });
         root.setStyle("-fx-background-image: url('sample/img/spaceBackground.png')");
         primaryStage.setTitle("#STELLAR");
         this.screenWidth = 1200;
